445f708e516147d7c1d18b329ab804dde5a3934c
SERENA FERRARO <serenaferraro21@gmail.com>
2024-10-21 15:03:33 +0200

    css and button

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,3 +1,5 @@
 const checkBox = document.getElementById('checkbox')
 const button = document.getElementById('proceedBtn')
 
+
+button.addEventListener('click', ()=>{window.location.href="benchmark.html"})
