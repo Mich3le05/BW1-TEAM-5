d58d3d72f053503b638255165046b41b029fa596
Michele Mandanici <michimandanici@tim.it>
2024-10-21 18:19:22 +0200

    edit result page

---
+++ script/result.js
@@ -0,0 +1,49 @@
+const calcola = function () {
+  let somma = 5 + 10
+  return somma
+}
+
+const data = {
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [calcola(), 100],
+      backgroundColor: ['#D20094', '#00FFFF'],
+      borderWidth: 0,
+    },
+  ],
+}
+
+const config = {
+  type: 'doughnut',
+  data: data,
+  options: {
+    cutout: '70%',
+    plugins: {
+      beforeDraw: function (chart) {
+        const ctx = chart.ctx
+        const chartArea = chart.chartArea
+
+        // Disegna il cerchio esterno con l'ombra
+        ctx.save()
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.2)' // Regola l'opacità
+        ctx.shadowBlur = 10 // Regola l'intensità del blur
+        ctx.shadowOffsetX = 10
+        ctx.shadowOffsetY = 10
+        ctx.beginPath()
+        ctx.arc(
+          chartArea.left + chartArea.width / 2,
+          chartArea.top + chartArea.height / 2,
+          chartArea.width / 2,
+          0,
+          2 * Math.PI
+        )
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.2)' // Regola il colore dell'ombra
+        ctx.fill()
+        ctx.restore()
+      },
+    },
+  },
+}
+
+var myChart = new Chart(document.getElementById('myChart'), config)
