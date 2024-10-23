3e7c175033826b2d8e7e6fcbb97621c2505a007b
Michele Mandanici <michimandanici@tim.it>
2024-10-23 12:20:29 +0200

    modifiche scritta dentro al grafico, colori ecc.

--- scripts/result.js
+++ scripts/result.js
@@ -5,7 +5,7 @@ const ctx = document.getElementById('myDonutChart').getContext('2d')
 const myDonutChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ['Vero', 'Falso'],
+    labels: ['Wrong', 'Correct'],
     datasets: [
       {
         label: 'Risultati',
@@ -28,23 +28,30 @@ const myDonutChart = new Chart(ctx, {
 
 // FINE GRAFICO
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   document.getElementById('DivChangeText').innerHTML = 'ciao'
-// })
-
-// const recuperatesto = function () {
-//   let retrievedArray = JSON.parse(localStorage.getItem('sharedArray'))
-//   console.log(retrievedArray)
-// }
-
-// recuperatesto()
 document.addEventListener('DOMContentLoaded', function () {
   const retrievedData = JSON.parse(localStorage.getItem('quizResults'))
   console.log(retrievedData)
   // Aggiorna il grafico con i dati recuperati
   myDonutChart.data.datasets[0].data = [
-    retrievedData.filter((result) => result === 1).length,
     retrievedData.filter((result) => result === 0).length,
+    retrievedData.filter((result) => result === 1).length,
   ]
   myDonutChart.update()
+
+  // Aggiorna il grafico e la scritta al centro
+  const chartTextElement = document.getElementById('DivChangeText')
+  myDonutChart.update()
+  const correctAnswers = retrievedData.filter((result) => result === 1).length
+  const totalAnswers = retrievedData.length
+  const percentageCorrect = (correctAnswers / totalAnswers) * 100
+
+  let message
+  if (percentageCorrect >= 60) {
+    message = `Congratulations! You passed the exam. You have answered ${percentageCorrect}% of the questions correctly!`
+  } else {
+    message = `Oops! You failed the exam. You only answered ${percentageCorrect}% of the questions correctly!`
+  }
+
+  chartTextElement.textContent = message
+  chartTextElement.classList.add(percentageCorrect >= 60 ? 'success' : 'fail')
 })
