bb60b5a67073d5e189cfa5cfe477420b618a259d
MARK-GHOBRIAL <Mark_emad8@yahoo.com>
2024-10-23 10:24:59 +0200

    adds

--- scripts/result.js
+++ scripts/result.js
@@ -1,40 +1,26 @@
-const ctx = document.getElementById('myDonutChart').getContext('2d');
+const ctx = document.getElementById('myDonutChart').getContext('2d')
 
 const myDonutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Vero', 'Falso'],
-        datasets: [{
-            label: 'Risultati',
-            data: [20, 10],
-            backgroundColor: [
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(255, 99, 132, 0.6)',
-            ],
-            borderColor: [
-                'rgba(75, 192, 192, 1)',
-                'rgba(255, 99, 132, 1)',
-            ],
-            borderWidth: 1
-        }]
+  type: 'doughnut',
+  data: {
+    labels: ['Vero', 'Falso'],
+    datasets: [
+      {
+        label: 'Risultati',
+        data: [20, 10],
+        backgroundColor: ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)'],
+        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    cutout: '90%',
+    plugins: {
+      legend: {
+        display: false,
+      },
     },
-    options: {
-        responsive: true,
-        cutout: '90%',
-        plugins: {
-            legend: {
-                display: false 
-            }
-        }
-    }
-});
-
-const calcolatesto = function {
-
-  if(){}
-  else{}
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("DivChangeText").innerHTML = calcolatesto();
-});
+  },
+})
